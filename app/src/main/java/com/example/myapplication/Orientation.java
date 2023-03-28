154294cb278c7de892d29dacfae54b85d815fd89
kamalrajd <kamalrajd@arista.com>
2023-03-28 20:01:47 +0530

    Change the action bar name in orientation page

--- app/src/main/java/com/example/myapplication/Orientation.java
+++ app/src/main/java/com/example/myapplication/Orientation.java
@@ -27,6 +27,11 @@ public class Orientation extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orientation);
+        // setting title in the action bar
+        try {
+            getSupportActionBar().setTitle("Orientation");
+        } catch (NullPointerException ignored) {}
+
         Spinner spinner_date,spinner_day,spinner_month,spinner_year;
         String [] Day_arr={"Days","Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
         String [] Date_arr={"Date","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"};
