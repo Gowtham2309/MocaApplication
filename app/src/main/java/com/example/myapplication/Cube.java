3e9bde091673c54332a5515762e6aca8a77ea810
kamalrajd <kamalrajd@arista.com>
2023-04-13 22:42:40 +0530

    Add cube drawing page

--- app/src/main/java/com/example/myapplication/Cube.java
+++ app/src/main/java/com/example/myapplication/Cube.java
@@ -1,5 +1,8 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -12,34 +15,25 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Calendar;
 
-public class cdt_Drawing extends AppCompatActivity {
-    private ImageView imageView,imageView_clr;
-
-    private float float_startX = -1, float_startY = -1,
-            float_endX = -1, float_endY = -1;
-
-    private Bitmap bit_map;
-    private Canvas canva;
+public class Cube extends AppCompatActivity {
+    ImageView clear, canvas;
+    private float float_startX = -1, float_startY = -1, float_endX = -1, float_endY = -1;
     private Paint paint = new Paint();
-    private Button button;
-    private View rootView;
+    private Bitmap bit_map;
+    Canvas canva;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cdt_drawing);
+        setContentView(R.layout.activity_cube);
 
         ActivityCompat.requestPermissions(this
                 ,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
@@ -47,32 +41,25 @@ public class cdt_Drawing extends AppCompatActivity {
                 PackageManager.PERMISSION_GRANTED);
         // setting title in the action bar
         try {
-            getSupportActionBar().setTitle("Clock Drawing Test");
+            getSupportActionBar().setTitle("Cube Drawing Test");
         } catch (NullPointerException ignored) {}
 
-        imageView = findViewById(R.id.imageView5);
-        imageView_clr=findViewById(R.id.imageView4);
+        canvas = findViewById(R.id.cubeCanvas);
+        clear =findViewById(R.id.cubeClear);
 
-        imageView_clr.setOnClickListener(new View.OnClickListener() {
+        clear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 canva.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
             }
         });
-
-//        rootView = findViewById(R.id.cdtRoot);
-//
-//        Utils.showInstruction(
-//                imageView_clr,
-//                "Draw a clock which shows time 10 past 11"
-//        );
     }
 
     private void drawPaintSketchImage(){
 
         if (bit_map == null){
-            bit_map = Bitmap.createBitmap(imageView.getWidth(),
-                    imageView.getHeight(),
+            bit_map = Bitmap.createBitmap(canvas.getWidth(),
+                    canvas.getHeight(),
                     Bitmap.Config.ARGB_8888);
             canva = new Canvas(bit_map);
             paint.setColor(Color.BLACK);
@@ -85,7 +72,7 @@ public class cdt_Drawing extends AppCompatActivity {
                 float_endX,
                 float_endY-220,
                 paint);
-        imageView.setImageBitmap(bit_map);
+        canvas.setImageBitmap(bit_map);
     }
 
 
@@ -117,23 +104,8 @@ public class cdt_Drawing extends AppCompatActivity {
 
         canva.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
     }
-
     public void buttonSaveImage(View view)
     {
-//        File fileSaveImage = new File(this.getExternalFilesDir(Environment.DIRECTORY_PICTURES),
-//                Calendar.getInstance().getTime().toString() + ".jpg");
-//        try {
-//            FileOutputStream fileOutputStream = new FileOutputStream(fileSaveImage);
-//            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream);
-//            fileOutputStream.flush();
-//            fileOutputStream.close();
-//            Toast.makeText(this,
-//                    "File Saved Successfully",
-//                    Toast.LENGTH_LONG).show();
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
         File file = new File(this.getExternalFilesDir(Environment.DIRECTORY_PICTURES), Calendar.getInstance().getTime().toString() + ".png");
         try
         {
@@ -148,9 +120,8 @@ public class cdt_Drawing extends AppCompatActivity {
             Toast.makeText(this, "Error saving image", Toast.LENGTH_SHORT).show();
             e.printStackTrace();
         }
-        Intent intent=new Intent(cdt_Drawing.this,Cube.class);
+        Intent intent=new Intent(this,gaming.class);
         startActivity(intent);
         finish();
     }
-
 }
